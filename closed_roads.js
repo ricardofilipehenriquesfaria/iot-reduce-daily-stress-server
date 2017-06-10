194de99087f743102f80a35466e705895dcf02c1
ricardofilipehenriquesfaria <ricardofaria@outlook.pt>
2017-06-10 03:03:16 +0100

    - Added connection to Socket.IO clients

--- closed_roads.js
+++ closed_roads.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var mysql = require('mysql');
+var io = require('socket.io');
 
 var connection = mysql.createConnection({
 	host : 'localhost',
@@ -8,20 +9,33 @@ var connection = mysql.createConnection({
 	database : 'closed_roads',
 });
 
-console.log('Detalhes da conexão MySQL: ' + connection);
-
-http.createServer(function(request, response) {
+var server = http.createServer(function(request, response){ 
     
-	console.log('Criando o Servidor HTTP...');
-    
-    connection.query('SELECT * FROM localizacao', function(err, results, fields) {
+	connection.query('SELECT * FROM localizacao', function(err, results, fields) {
         
-        console.log('Connection result error: ' + err);
 		console.log('Número de Registos: ' + results.length);
+		
 		response.writeHead(200, { 'Content-Type': 'application/json'});
 		response.end(JSON.stringify(results));
 		response.end();
 
     });
+}).listen(3000);
 
-}).listen(3000, "0.0.0.0");
+
+var socket = io.listen(server);
+
+socket.on('connection', function(client){ 
+    
+	console.log('Ligação estabelecida com o cliente!');
+
+    client.on('message', function(event){ 
+        console.log('Mensagem recebida do cliente:', event);
+    });
+
+    client.on('disconnect',function(){
+        console.log('Ligação perdida!');
+    });
+});
+
+console.log('Servidor correndo em http://127.0.0.1:3000/');
