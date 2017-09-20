bd2675eeba14485fdd399f169e4bc1ab47fc0ce2
ricardofilipehenriquesfaria <ricardofaria@outlook.pt>
2017-09-20 23:34:24 +0100

    - Correction of some bugs

--- mysql/mysql_width.js
+++ mysql/mysql_width.js
@@ -14,13 +14,13 @@ module.exports = {
 		var query = "";
 		if(numberParameters > 1){
 			for(var i = 0; i < numberParameters; i++){
-				query = query + "SELECT * FROM funchal_roads WHERE id = (SELECT id FROM bounding_boxes WHERE southLatitude <= '" + queryData.latitude[i]
+				query = query + "SELECT * FROM funchal_roads WHERE id = (SELECT id_funchal_roads FROM bounding_boxes WHERE southLatitude <= '" + queryData.latitude[i]
 					+ "' AND northLatitude >= '" + queryData.latitude[i]
 					+ "' AND westLongitude <= '" + queryData.longitude[i]
 					+ "' AND eastLongitude >= '" + queryData.longitude[i] + "' LIMIT 1);";		
 			}
 		} else {
-			query = query + "SELECT * FROM funchal_roads WHERE id = (SELECT id FROM bounding_boxes WHERE southLatitude <= '" + queryData.latitude
+			query = query + "SELECT * FROM funchal_roads WHERE id = (SELECT id_funchal_roads FROM bounding_boxes WHERE southLatitude <= '" + queryData.latitude
 					+ "' AND northLatitude >= '" + queryData.latitude
 					+ "' AND westLongitude <= '" + queryData.longitude
 					+ "' AND eastLongitude >= '" + queryData.longitude + "' LIMIT 1);";	
@@ -35,12 +35,15 @@ module.exports = {
 				for(var i = 0; i < results.length; i++){
 					var string = JSON.stringify(results[i]);
 					string = string.replace(/[\[\]']+/g,'');
+					
+					if(JSON.stringify(string) === JSON.stringify('')){
+						string = "{\"id\":0,\"toponimo\":\"\",\"categoria\":\"\",\"tipo_uso\":\"\",\"extensao_via\":0,\"largura_via\":0,\"tipo_pavimento\":\"\",\"estado_conservacao\":\"\"}";
+					}
 					array.push(string);
 				}
-				
 				callback(JSON.stringify(array));
 			} else {
-				callback(JSON.stringify([0]));
+				callback(JSON.stringify(["{\"id\":0,\"toponimo\":\"\",\"categoria\":\"\",\"tipo_uso\":\"\",\"extensao_via\":0,\"largura_via\":0,\"tipo_pavimento\":\"\",\"estado_conservacao\":\"\"}"]));
 			}
 		});
 	}
